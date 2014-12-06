4000f0b49ab451efdcb151d56053649dd10d267b
HKLCF <hklcf@users.noreply.github.com>
2014-12-06 18:42:54 +0800

    Update config.php

--- config.php
+++ config.php
@@ -13,23 +13,23 @@ $lang = strtolower($_GET['lang']);
 $_lang = (!$_COOKIE['lang'] || !in_array($_COOKIE['lang'], array('zh', 'en'))) ? 'zh' : $_COOKIE['lang'];
 $urlparm = $action ? "?action=$action" : '';
 if(isset($lang) && in_array($lang, array('zh', 'en'))) {
-	setcookie('lang', $lang);
-	header("Location: index.php$urlparm");
+    setcookie('lang', $lang);
+    header("Location: index.php$urlparm");
 }
 $userlang = $_lang == 'zh' ? '繁中' : 'Eng';
 include './lang/'.$_lang.'.php';
 
 /*
 if($_SERVER["HTTP_CF_VISITOR"] != '{"scheme":"https"}') {
-	Header("Location: https://{$_SERVER["HTTP_HOST"]}{$_SERVER["PHP_SELF"]}$urlparm");
+    Header("Location: https://{$_SERVER["HTTP_HOST"]}{$_SERVER["PHP_SELF"]}$urlparm");
 }
 */
 if($_SERVER["HTTP_CF_VISITOR"] != '{"scheme":"https"}') {
-	$ssl = "https";
-	$ssl_url = "https://{$_SERVER["HTTP_HOST"]}{$_SERVER["PHP_SELF"]}$urlparm";
+    $ssl = "https";
+    $ssl_url = "https://{$_SERVER["HTTP_HOST"]}{$_SERVER["PHP_SELF"]}$urlparm";
 } else {
-	$ssl = "http";
-	$ssl_url = "http://{$_SERVER["HTTP_HOST"]}{$_SERVER["PHP_SELF"]}$urlparm";
+    $ssl = "http";
+    $ssl_url = "http://{$_SERVER["HTTP_HOST"]}{$_SERVER["PHP_SELF"]}$urlparm";
 }
 
 //System Setting
@@ -54,13 +54,13 @@ $to = 'demo@demo.com';
 
 //Setting the administor account of system
 function encrypt_pass($password) {
-	$key = '1c5344c9a85b00a7db02afcb88debc41';
-	$result = md5(md5($password).$key);
-	return $result;
+    $key = '1c5344c9a85b00a7db02afcb88debc41';
+    $result = md5(md5($password).$key);
+    return $result;
 }
 
 $admin = array(
-	'username'	=>	'demo',
-	'password'	=>	'1c5344c9a85b00a7db02afcb88debc41'
+    'username'	=>	'demo',
+    'password'	=>	'1c5344c9a85b00a7db02afcb88debc41'
 );
 ?>
