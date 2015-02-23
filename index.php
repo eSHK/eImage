043e2bb795be605bbdd74b654d7f09aedf9fcba9
HKLCF <hklcf@users.noreply.github.com>
2015-02-23 22:42:21 +0800

    Update index.php

--- index.php
+++ index.php
@@ -49,7 +49,8 @@ if($_GET['action'] == 'contact' && isset($_GET['send'])) {
 
 if($_GET['action'] == 'login' && isset($_GET['send'])) {
     if($_POST['username'] == $admin['username'] && encrypt_pass($_POST['password']) == $admin['password']) {
-        $content = 'api.html';
+        setcookie('login', 1)
+        $content = 'chart.html';
     } else {
         $content = 'upload.html';
     }
