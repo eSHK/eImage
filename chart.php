ba0f8c98de9b84e92d845e559b419882d0b40fcd
HKLCF <hklcf@users.noreply.github.com>
2015-02-23 21:28:25 +0800

    Update chart.php

--- chart.php
+++ chart.php
@@ -1,6 +1,23 @@
 <?php
 require '.config.php';
 
-
+$id = htmlentities($_GET['id']);
+if(isset($id) && is_numeric($id)) {
+    switch($id) {
+        case 1:
+            $chart_url = '';
+            break;
+        case 2:
+            $chart_url = '';
+            break;
+        case 3:
+            $chart_url = '';
+            break;
+        case 4:
+            $chart_url = '';
+            break;
+    }
+    file_get_contents();
+}
 
 ?>
