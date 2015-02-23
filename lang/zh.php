9bdbdf473852746facaaf243f1b85575f971dbc3
HKLCF <hklcf@users.noreply.github.com>
2015-02-23 22:34:32 +0800

    Update zh.php

--- lang/zh.php
+++ lang/zh.php
@@ -5,77 +5,77 @@ define('CSSFILE', 'style.css');
 define('JSFILE', 'system.js');
 $langpark = array(
 
-    //Index page
-    'login'                    =>    '管理員登入',
-    'api'                    =>    'API',
-    'chart'                 =>    '圖片',
-    'announcement'            =>    '網站公告',
-    'home'                    =>    '首頁',
-    'home_desc'                =>    'eImage 是一個免費圖片上傳服務，讓您輕鬆上傳、鏈接和分享您的圖片在論壇、博客並與您的朋友們在一個快速，穩定和便捷的服務中上傳圖片。',
-    'terms_alert'            =>    '使用本網站的上傳圖片服務前，請先確定你已閱讀本網站的 <a href="?action=terms">條款及細則</a>。成人級別的圖片是不允許及會被刪除的。',
-    'filetypes'                =>    '允許檔案類別：',
-    'bandwidth'                =>    '流量限制：',
-    'filesize'                =>    '檔案大小限制：',
-    'user'                    =>    '需要註冊：',
-    'unlimited'                =>    '無限',
-    'notrequire'            =>    '不需要',
-    'fileuploading'            =>    '檔案上傳中...',
-    'upload_button'            =>    '上傳檔案',
-    'about'                    =>    '關於我們',
-    'contact'                =>    '聯絡我們',
-    'faq'                    =>    '常見問題',
-    'terms'                    =>    '條款及細則',
-    'notfound'                =>    '<span>&nbsp;&nbsp;</span><b>找不到網頁</b><br /><span>&nbsp;&nbsp;<a href="index.php">回到首頁</a></span>',
+	//Index page
+	'login'					=>	'管理員登入',
+	'api'					=>	'API',
+	'chart'					=>	'圖表',
+	'announcement'			=>	'網站公告',
+	'home'					=>	'首頁',
+	'home_desc'				=>	'eImage 是一個免費圖片上傳服務，讓您輕鬆上傳、鏈接和分享您的圖片在論壇、博客並與您的朋友們在一個快速，穩定和便捷的服務中上傳圖片。',
+	'terms_alert'			=>	'使用本網站的上傳圖片服務前，請先確定你已閱讀本網站的 <a href="?action=terms">條款及細則</a>。成人級別的圖片是不允許及會被刪除的。',
+	'filetypes'				=>	'允許檔案類別：',
+	'bandwidth'				=>	'流量限制：',
+	'filesize'				=>	'檔案大小限制：',
+	'user'					=>	'需要註冊：',
+	'unlimited'				=>	'無限',
+	'notrequire'			=>	'不需要',
+	'fileuploading'			=>	'檔案上傳中...',
+	'upload_button'			=>	'上傳檔案',
+	'about'					=>	'關於我們',
+	'contact'				=>	'聯絡我們',
+	'faq'					=>	'常見問題',
+	'terms'					=>	'條款及細則',
+	'notfound'				=>	'<span>&nbsp;&nbsp;</span><b>找不到網頁</b><br /><span>&nbsp;&nbsp;<a href="index.php">回到首頁</a></span>',
 
-    //Upload page
-    'filename'                =>    '圖片名稱：',
-    'filetype'                =>    '類型：',
-    'photosize'                =>    '檔案大小：',
-    'resolution'            =>    '解析度：',
-    'savepath'                =>    '儲存位置：',
-    'html_code'                =>    'HTML 代碼',
-    'thumb_code'            =>    '縮圖代碼',
-    'img_code'                =>    'IMG 代碼',
-    'direct_link'            =>    '直接鏈結',
-    'upload_another_pic'    =>    '上傳另一幅圖片',
-    'open_new_window'        =>    '在新視窗中開啟圖片',
-    'thumbsize'                =>    '縮圖大小',
-    'download'                =>    '下載',
+	//Upload page
+	'filename'				=>	'圖片名稱：',
+	'filetype'				=>	'類型：',
+	'photosize'				=>	'檔案大小：',
+	'resolution'			=>	'解析度：',
+	'savepath'				=>	'儲存位置：',
+	'html_code'				=>	'HTML 代碼',
+	'thumb_code'			=>	'縮圖代碼',
+	'img_code'				=>	'IMG 代碼',
+	'direct_link'			=>	'直接鏈結',
+	'upload_another_pic'	=>	'上傳另一幅圖片',
+	'open_new_window'		=>	'在新視窗中開啟圖片',
+	'thumbsize'				=>	'縮圖大小',
+	'download'				=>	'下載',
 
-    //Upload Error
-    'file_exist'            =>    '已經存在',
-    'upload_error'            =>    '上傳失敗！',
-    'file_size_large'        =>    '檔案容量太大！',
-    'not_support_format'    =>    '不支援此檔案格式！',
-    'cannot_detect_size'    =>    '不能檢測檔案大小！',
-    
-    //Show Image Error
-    'open_error'            =>    '開啟檔案錯誤！',
-    'resize_large'            =>    '圖片大小調整不能大於',
-    'pixel'                    =>    '像素',
-    
-    //Contact page
-    'type'                    =>    '問題類別：',
-    'name'                    =>    '姓名：',
-    'email'                    =>    'E-mail：',
-    'message'                =>    '訊息：',
-    'link'                    =>    '回報問題連結：',
-    'ip'                    =>    'IP地址：',
-    'ua'                    =>    '用戶代理：',
-    
-    //Type of contact
-    'type1'                    =>    '版權',
-    'type2'                    =>    '條款',
-    'type3'                    =>    '建議',
-    'type4'                    =>    '合作',
+	//Upload Error
+	'file_exist'			=>	'已經存在',
+	'upload_error'			=>	'上傳失敗！',
+	'file_size_large'		=>	'檔案容量太大！',
+	'not_support_format'	=>	'不支援此檔案格式！',
+	'cannot_detect_size'	=>	'不能檢測檔案大小！',
+	
+	//Show Image Error
+	'open_error'			=>	'開啟檔案錯誤！',
+	'resize_large'			=>	'圖片大小調整不能大於',
+	'pixel'					=>	'像素',
+	
+	//Contact page
+	'type'					=>	'問題類別：',
+	'name'					=>	'姓名：',
+	'email'					=>	'E-mail：',
+	'message'				=>	'訊息：',
+	'link'					=>	'回報問題連結：',
+	'ip'					=>	'IP地址：',
+	'ua'					=>	'用戶代理：',
+	
+	//Type of contact
+	'type1'					=>	'版權',
+	'type2'					=>	'條款',
+	'type3'					=>	'建議',
+	'type4'					=>	'合作',
 
-    //Login page
-    'username'                =>    '帳號：',
-    'password'                =>    '密碼：',
+	//Login page
+	'username'				=>	'帳號：',
+	'password'				=>	'密碼：',
 
-    //Other
-    'reupload'                =>    '重新上傳',
-    'close'                    =>    '關閉'
+	//Other
+	'reupload'				=>	'重新上傳',
+	'close'					=>	'關閉'
 );
 
 ?>
