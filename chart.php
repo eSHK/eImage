4d357f459f359729fbee50e82680be7c5d1bd649
HKLCF <hklcf@users.noreply.github.com>
2015-02-23 21:41:03 +0800

    Update chart.php

--- chart.php
+++ chart.php
@@ -4,16 +4,19 @@ require '.config.php';
 $id = htmlentities($_GET['id']);
 if(isset($id) && is_numeric($id)) {
     switch($id) {
-        case 1:
+        case 1: // eth0 traffic
             $chart_url = '';
             break;
-        case 2:
+        case 2: // CPU usage
             $chart_url = '';
             break;
-        case 3:
+        case 3: // Load average
             $chart_url = '';
             break;
-        case 4:
+        case 4: // Memory usage
+            $chart_url = '';
+            break;
+        case 5: // Disk IOs per device
             $chart_url = '';
             break;
     }
